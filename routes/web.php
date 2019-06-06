fb492dee2a2b594e9494008f00af73226bf6ffb1
Emanuel Rybak <ery@pc-tec.local>
2019-06-06 09:28:38 +0200

    Added and finished blocking functionality. In the user model i added a private method called filter_blocked_users. This method should be run on every collection of users that is mean to be returned to a user

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,17 @@ Route::get('/index', 'LikeController@index')->name('like.index');
 
 Route::get('/index/likes', 'LikeController@likes')->name('likes');
 
+Route::get('/user/blocks', 'BlockController@index')->name('blocks');
+
 Route::get('/home', 'HomeController@index')->name('home');
 
 
 Route::post('/like', 'LikeController@store')->name('like');
 
+Route::post('/block', 'BlockController@store')->name('block.store');
+
 Route::delete('/like', 'LikeController@destroy')->name('like.destroy');
 
+Route::delete('/block', 'BlockController@destroy')->name('block.destroy');
 
 Auth::routes();
